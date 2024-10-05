c349c36afeffb7b945df4ee6d5bf1deb79ee05dc
wangxun <1013296326@qq.com>
2024-10-05 22:07:29 +0800

    up init

--- pulasan/__init__.py
+++ pulasan/__init__.py
@@ -7,36 +7,11 @@ from pulasan.configs import settings
 def create_app():
     app = Quart('pulasan')
 
-    register_blueprints(app)
-    register_errors(app)
     register_events(app)
     register_middlewares(app)
+    register_errors(app)
+    register_blueprints(app)
     return app
-
-
-def register_blueprints(app):
-    from pulasan.urls import user_url, chat_url, admin_url
-
-    app.register_blueprint(chat_url.chat_url)
-    app.register_blueprint(user_url.user_url, url_prefix='/user')
-    app.register_blueprint(admin_url.admin_url, url_prefix='/admin')
-
-
-def register_errors(app):
-    from pulasan.models.log_model import LogModel
-    from pulasan.utils.tools import abort
-
-    @app.errorhandler(HTTPException)
-    async def http_exception_handler(error):
-        # error_code = getattr(error, 'code', 500)
-        # message = HTTP_STATUS_CODES.get(error_code, str(error))
-        # error_code = error.code
-        return abort(error.code)
-
-    @app.errorhandler(Exception)
-    async def global_exception_handler(error):
-        await LogModel().exception(error)
-        return abort(500)
 
 
 def register_events(app):
@@ -58,6 +33,31 @@ def register_middlewares(app):
     from pulasan.middlewares import cors_middleware
 
     cors_middleware.init_app(app)
+
+
+def register_errors(app):
+    from pulasan.models.log_model import LogModel
+    from pulasan.utils.tools import abort
+
+    @app.errorhandler(HTTPException)
+    async def http_exception_handler(error):
+        # error_code = getattr(error, 'code', 500)
+        # message = HTTP_STATUS_CODES.get(error_code, str(error))
+        # error_code = error.code
+        return abort(error.code)
+
+    @app.errorhandler(Exception)
+    async def global_exception_handler(error):
+        await LogModel().exception(error)
+        return abort(500)
+
+
+def register_blueprints(app):
+    from pulasan.urls import user_url, chat_url, admin_url
+
+    app.register_blueprint(chat_url.chat_url)
+    app.register_blueprint(user_url.user_url, url_prefix='/user')
+    app.register_blueprint(admin_url.admin_url, url_prefix='/admin')
 
 
 app = create_app()
